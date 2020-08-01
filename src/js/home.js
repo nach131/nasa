d3acb52166eeec53c976e79856cdd0bf4efea16d
nacho <nacho.mota@gmail.com>
2020-08-01 17:10:12 +0200

    Album

--- src/js/home.js
+++ src/js/home.js
@@ -14,27 +14,30 @@ const marteList = await getDatos("https://images-api.nasa.gov/search?q=Opportuni
 
 
 
-function FotoItemTemplate(marte){
-  return(
-`<div class="card">
-<img src="${marte.links[0].href}" alt="...">
-<div class="card-body">
-  <h5 class="card-title mb-0">${marte.data[0].nasa_id}</h5>
-  <div class="badges mb-2">
-    <span class="badge badge-warning">${marte.data[0].keywords}</span>
-    <span class="badge badge-danger">KTM</span>
-  </div>
-  <p class="card-text">${marte.data[0].description}</p>
-</div>
-</div>`
+// function FotoItemTemplate(marte){
+//   return(
+// `<div class="card">
+// <img src="${marte.links[0].href}" alt="...">
+// <div class="card-body">
+//   <h5 class="card-title mb-0">${marte.data[0].nasa_id}</h5>
+//   <div class="badges mb-2">
+//     <span class="badge badge-warning">${marte.data[0].keywords}</span>
+//     <span class="badge badge-danger">KTM</span>
+//   </div>
+//   <p class="card-text">${marte.data[0].description}</p>
+// </div>
+// </div>`
 
-  )
-}
-
+//   )
+// }
+const $fotosContenedor = document.querySelector('#fotos')
 
 marteList.collection.items.forEach((marte)=>{
-  console.log(marte)
+  // console.log(marte)
   const HTMLString = FotoItemTemplate(marte)
+  const html = document.implementation.createHTMLDocument()
+  html.body.innerHTML = HTMLString
+  $fotosContenedor.append(html.body.children[0])
   console.log(HTMLString)
 })
 
