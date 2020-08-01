3c1e7966deff0ab6cf241c6530ff11c25553d8a7
nacho <nacho.mota@gmail.com>
2020-08-01 15:29:40 +0200

    Inicio

---
+++ src/js/home.js
@@ -0,0 +1,43 @@
+console.log('hola mundo!');
+
+(async function load(){
+
+async function getDatos(url) {
+  const respuesta = await fetch(url)
+  const datos = await respuesta.json()
+  return datos
+}
+const marteList = await getDatos("https://images-api.nasa.gov/search?q=Opportunity&page=2")
+// debugger
+// console.log(marteList)
+
+
+
+
+function FotoItemTemplate(marte){
+  return(
+`<div class="card">
+<img src="${marte.links[0].href}" alt="...">
+<div class="card-body">
+  <h5 class="card-title mb-0">${marte.data[0].nasa_id}</h5>
+  <div class="badges mb-2">
+    <span class="badge badge-warning">${marte.data[0].keywords}</span>
+    <span class="badge badge-danger">KTM</span>
+  </div>
+  <p class="card-text">${marte.data[0].description}</p>
+</div>
+</div>`
+
+  )
+}
+
+
+marteList.collection.items.forEach((marte)=>{
+  console.log(marte)
+  const HTMLString = FotoItemTemplate(marte)
+  console.log(HTMLString)
+})
+
+
+
+})()
