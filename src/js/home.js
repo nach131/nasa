839f8a3dc547a6e064da66dcc374cb6a6e599fad
nacho <nacho.mota@gmail.com>
2020-08-02 14:54:09 +0200

    Renombrado las criterios funciones

--- src/js/home.js
+++ src/js/home.js
@@ -7,23 +7,23 @@ async function getDatos(url) {
   const datos = await respuesta.json()
   return datos
 }
-const marteList = await getDatos("https://images-api.nasa.gov/search?q=Opportunity&page=3")
+const buscarList = await getDatos("https://images-api.nasa.gov/search?q=dragon&page=3")
 // debugger
-// console.log(marteList)
+// console.log(buscarList)
 
 
-function FotoItemTemplate(marte){
+function FotoItemTemplate(foto){
   return(
     `<div class="col-xl-3 col-md-4">
     <div class="card mb-4 shadow-sm" >
-    <img src="${marte.links[0].href}" class="card-img-top" alt="..." hidden>
-    <div class="card-bg-img" style="background-image:url(${marte.links[0].href});"></div>
+    <img src="${foto.links[0].href}" class="card-img-top" alt="..." hidden>
+    <div class="card-bg-img" style="background-image:url(${foto.links[0].href});"></div>
     <div class="card-body">
-      <h5 class="card-title mb-0">${marte.data[0].nasa_id}</h5>
+      <h5 class="card-title mb-0">${foto.data[0].nasa_id}</h5>
       <div class="badges mb-2">
-        <span class="badge badge-warning">${marte.data[0].keywords}</span>
+        <span class="badge badge-warning">${foto.data[0].keywords}</span>
       </div>
-      <p class="card-text">${marte.data[0].description}</p>
+      <p class="card-text">${foto.data[0].description}</p>
     </div>
     </div>
   </div>`
@@ -36,9 +36,9 @@ function CrearPlantilla(HTMLString){
 }
 
 const $fotosContenedor = document.querySelector('#fotos')
-marteList.collection.items.forEach((marte)=>{
-  // console.log(marte)
-  const HTMLString = FotoItemTemplate(marte)
+buscarList.collection.items.forEach((lista)=>{
+  // console.log(lista)
+  const HTMLString = FotoItemTemplate(lista)
   const AlbumItems = CrearPlantilla(HTMLString)
   $fotosContenedor.append(AlbumItems)
   // console.log(HTMLString)
