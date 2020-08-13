dc57203e350636618b697cae11187118e4009180
nacho <nacho.mota@gmail.com>
2020-08-13 16:08:35 +0200

    onclick buscar quita las fotos

--- src/js/home.js
+++ src/js/home.js
@@ -1,23 +1,26 @@
-(async function load() {
-  async function getDatos(url) {
-    const respuesta = await fetch(url);
-    const datos = await respuesta.json();
-    return datos;
-  }
-  const $form = document.getElementById("form");
-  const $spinner = document.getElementById('spinner');
-  const BASE_API = 'https://images-api.nasa.gov/';
-  
-  
 
-  function FotoItemTemplate(foto) {
-    //   var str = foto.data[0].date_created
-    // console.log(str ,str.substr(0, 10))
-    return `<div class="col-xl-3 col-md-4">
+async function getDatos(url) {
+  const respuesta = await fetch(url);
+  const datos = await respuesta.json();
+  return datos;
+}
+const $form = document.getElementById("form");
+const $spinner = document.getElementById('spinner');
+const BASE_API = 'https://images-api.nasa.gov/';
+
+const $aviso = document.getElementById('aviso');
+const $fotos = document.getElementById('fotos');
+
+
+
+function FotoItemTemplate(foto) {
+  //   var str = foto.data[0].date_created
+  // console.log(str ,str.substr(0, 10))
+  return `<div class="col-xl-3 col-md-4">
     <div class="card mb-4 shadow-sm" >
     <img src="${foto.links[0].href}" class="card-img-top" alt="..." hidden>
     <div class="card-bg-img" style="background-image:url(${foto.links[0]
-        .href});"></div>
+      .href});"></div>
     <div class="card-body">
       <h5 class="card-title mb-0">${foto.data[0].nasa_id}</h5>
       <span class="badge date">${foto.data[0].date_created.substr(0, 10)}</span>
@@ -28,81 +31,119 @@
     </div>
     </div>
   </div>`;
-  }
-  function CrearPlantilla(HTMLString) {
-    const html = document.implementation.createHTMLDocument();
-    html.body.innerHTML = HTMLString;
-    return html.body.children[0];
-  }
+}
+function CrearPlantilla(HTMLString) {
+  const html = document.implementation.createHTMLDocument();
+  html.body.innerHTML = HTMLString;
+  return html.body.children[0];
+}
+
+const $carousel = document.getElementById('carousel');
+const $fotosContenedor = document.querySelector("#fotos");
 
 
-  const $carousel = document.getElementById('carousel');
-
-
-
-  const $fotosContenedor = document.querySelector("#fotos");
-
- /* =====================================
-          BUSCAR
+/* =====================================
+         BUSCAR
 ======================================*/
-  $form.addEventListener("submit",async event => {
-    event.preventDefault()
-    $spinner.classList.add('lds-hourglass', 'mx-auto', 'd-block');
-    const data = new FormData($form)
-    // const buscado = await getDatos(`${BASE_API}search?q=${data.get('buscar')}`)
-    const {
-      collection:{
-        items: buscado
-      }
-    } = await getDatos(`${BASE_API}search?q=${data.get('buscar')}`)
-    // debugger
-    renderAlbumList(buscado, $fotosContenedor);
-    // ElementoEncontrado(buscado.collection.metadata.total_hits);
-  })
-
-  function renderAlbumList(lista, $contenedor) {
-    $carousel.setAttribute("hidden", "");
-    $contenedor.children[0].remove(); // quitar el spiner
-    // document.querySelector('#fotos').remove()
-    lista.forEach(lista => {
-      // console.log(lista)
-      const HTMLString = FotoItemTemplate(lista);
-      const AlbumItems = CrearPlantilla(HTMLString);
-      $contenedor.append(AlbumItems);
-      addEventClick(AlbumItems);
-      // console.log(HTMLString)
-      // console.log(HTMLString)
-    });
+function setAtributos($elemento, atributos) {
+  for (const atributo in atributos) {
+    $elemento.setAttribute(atributo, atributos[atributo]);
   }
-  // renderAlbumList(buscarList.collection.items, $fotosContenedor);
+}
 
+const $loader = document.createElement("div");
 
-  /* =====================================
-          MODAL
+$fotos.append($loader);
+setAtributos($loader, {
+  id: "spinner",
+  role: "status"
+});
+
+$form.addEventListener("submit", async event => {
+  event.preventDefault()
+  // $spinner.classList.add('lds-hourglass', 'mx-auto', 'd-block');
+  $spinner.classList.add('spinner-border');
+  const data = new FormData($form)
+  // const buscado = await getDatos(`${BASE_API}search?q=${data.get('buscar')}`)
+  const {
+    collection: {
+      items: buscado
+    }
+  } = await getDatos(`${BASE_API}search?q=${data.get('buscar')}`)
+  // debugger
+  renderAlbumList(buscado, $fotosContenedor);
+  // ElementoEncontrado(buscado.collection.metadata.total_hits);
+})
+
+function renderAlbumList(lista, $contenedor) {
+  $carousel.setAttribute("hidden", "");
+  $contenedor.children[0].remove(); // quitar el spiner
+  // document.querySelector('#fotos').remove()
+  lista.forEach(lista => {
+    // console.log(lista)
+    const HTMLString = FotoItemTemplate(lista);
+    const AlbumItems = CrearPlantilla(HTMLString);
+    $contenedor.append(AlbumItems);
+    addEventClick(AlbumItems);
+    // console.log(HTMLString)
+    // console.log(HTMLString)
+  });
+}
+
+const $input = document.getElementById("input");
+$input.onmouseover = function () {
+  $input.value = '';
+
+  // $fotos.remove(); // quitar el spiner
+
+}
+
+/* =====================================
+      Quita Hijos de Album
 ======================================*/
-  function addEventClick($element) {
-    $element.addEventListener("click", () => {
-      // alert("clikazo");
-      showModal();
-    });
+function removeAllChildFotos(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.lastChild);
   }
+}
 
-  const $modal = document.getElementById("modal");
-  const $overlay = document.getElementById("overlay");
-  // const $cerrarModal = document.getElementById("cerrar-modal");
-  const $cerrarModal = document.getElementById("modal");
+$input.onclick = function () {
+  removeAllChildFotos($fotos)
+  $fotos.append($loader);
+  setAtributos($loader, {
+    id: "spinner",
+    class: ""
+  });
+}
 
-  function showModal() {
-    $overlay.classList.add("active");
-    $modal.style.animation = "modalIn .8s forwards";
-  }
+/* =====================================
+        MODAL
+======================================*/
+function addEventClick($element) {
+  $element.addEventListener("click", () => {
+    // alert("clikazo");
+    showModal();
+  });
+}
 
-  $cerrarModal.addEventListener("click", cerrarModal);
-  function cerrarModal() {
-    $overlay.classList.remove("active");
-    $modal.style.animation = "modalOut .8s forwards";
-  }
-})();
+const $modal = document.getElementById("modal");
+const $overlay = document.getElementById("overlay");
+// const $cerrarModal = document.getElementById("cerrar-modal");
+const $cerrarModal = document.getElementById("modal");
+
+function showModal() {
+  $overlay.classList.add("active");
+  $modal.style.animation = "modalIn .8s forwards";
+}
+
+$cerrarModal.addEventListener("click", cerrarModal);
+function cerrarModal() {
+  $overlay.classList.remove("active");
+  $modal.style.animation = "modalOut .8s forwards";
+}
+
+
+
 /* =====================================
           Activacion WOW
 ======================================*/
@@ -116,3 +157,7 @@ $(window).on('load', function () {
   $("#home-text").addClass("animated zoomIn");
   $("#home-btn").addClass("animated zoomIn");
 });
+
+
+
+
